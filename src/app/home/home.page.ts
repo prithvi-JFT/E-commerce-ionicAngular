862d017bcceb4bb322328521d08964e98778fc1a
prithvi raj <prithvi.raj@jellyfishtechnologies.com>
2021-06-03 17:34:36 -0400

    first commit from this branch

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-
+import {GetProductsService} from './get-products.service'
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  products:any=[]
+  constructor(private productsService:GetProductsService) {
+    this.productsService.getProducts().subscribe((data)=>{
+      this.products=data
+      console.log(data)
+    })
 
-  constructor() {}
+  }
+
 
 }
