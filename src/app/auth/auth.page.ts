ec3734b17026ef97ebef70b0844eb0f2d58e85e5
Adarsh Pal <adarsh.pal@jellyfishtechnologies.com>
2021-06-09 13:22:08 +0530

    changes in authentication

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { EmailValidator, NgForm } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { LoadingController } from '@ionic/angular';
+import { AlertController, LoadingController } from '@ionic/angular';
 import { AuthService } from './auth.service';
 
 @Component({
@@ -14,7 +14,7 @@ export class AuthPage implements OnInit {
   username: string
   password: string
   users: any = []
-  constructor(private http: HttpClient, private authService: AuthService, private router: Router, private loadingCtrl: LoadingController) { }
+  constructor(private http: HttpClient, private authService: AuthService, private router: Router, private loadingCtrl: LoadingController, private alertCtrl: AlertController) { }
 
   ngOnInit() {
     this.authService.getUsers().subscribe(data => {
@@ -30,21 +30,38 @@ export class AuthPage implements OnInit {
 
   onLogin() {
     this.authService.login(this.username, this.password).subscribe((res) => {
-      this.users.forEach(data=>{
-        if(data.username === this.username && data.password === this.password){
-          localStorage.setItem('username', data.username)
-          this.loadingCtrl.create({
-            keyboardClose: true,
-            message: 'Logging In...'
-          }).then(loadingEl => {
-            loadingEl.present()
-            setTimeout(() => {
-              loadingEl.dismiss()
-              this.router.navigate(['/home'])
-            }, 1000)
+      this.loadingCtrl.create({
+        keyboardClose: true,
+        message: 'Logging In...'
+      }).then(loadinEl => {
+        loadinEl.present()
+        setTimeout(() => {
+          let found = false
+          this.users.forEach(data => {
+            if(data.username === this.username && data.password === this.password) {
+              localStorage.setItem('username', data.username)
+              loadinEl.dismiss()
+              found = true
+            }
           })
-        }
-    })
+          if(found) {
+            this.router.navigate(['/home'])
+          } else {
+            this.alertCtrl.create({
+              message: 'Invalid Credentials',
+              buttons: [
+                {
+                text: 'Okay',
+                role: 'ok'
+                }
+              ]
+            }).then(alertEl => {
+              alertEl.present()
+              loadinEl.dismiss()
+            })
+          }
+        }, 1000)
       })
+    })
   }
 }
