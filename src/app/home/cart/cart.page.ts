a7087363faf5c9833ad7cdd774226b54f24eb42b
prithvi raj <prithvi.raj@jellyfishtechnologies.com>
2021-06-24 13:47:52 -0400

    cart price added

--- src/app/home/cart/cart.page.ts
+++ src/app/home/cart/cart.page.ts
@@ -16,15 +16,19 @@ export class CartPage implements OnInit {
   userId: number=+this.userid;
   buttonText='Back';
   deletedCart: any={};
+  priceArray: any=[];
 
-  constructor(private route: ActivatedRoute, private navCtrl: NavController, private cartService: CartService, private homeService: HomeService) { }
+  constructor(private route: ActivatedRoute,
+          private navCtrl: NavController,
+          private cartService: CartService,
+          private homeService: HomeService) {}
 
-  ngOnInit() {this.getCart();this.allProducts();}
+  async ngOnInit() {await this.getCart();await this.allProducts();}
 
   async getCart(){
     await this.cartService.getUserCart(this.userId).subscribe((cart) => {
         this.userCart = cart;
-        console.log('logging', this.userCart);
+        console.log('logging usercart', this.userCart);
       });
     }
 
@@ -32,21 +36,60 @@ export class CartPage implements OnInit {
     await this.homeService.fetchProducts().subscribe((products)=>{
       this.products=products;
       console.log(this.products);
+      this.getCartPrice();
     });
   }
 
-
-
-deleteFromCart(cartId) {
-  this.cartService.deleteCartProduct(cartId).subscribe(data => {
-    this.deletedCart=data;
-    console.log(this.deletedCart.id);
-    this.userCart.forEach((element ,index)=> {
-      if (element.id===this.deletedCart.id){
-        this.userCart.splice(index,1);
-        return;
-      }
+  getCartPrice(){
+    this.userCart.forEach((element)=> {
+      let totalPrice: any=0;
+      element.products.forEach((product,index) => {
+        totalPrice+= this.products[product.productId].price*product.quantity;
+      });
+      this.priceArray.push(totalPrice);
+      console.log(this.priceArray);
     });
-  });
+
+  };
+
+
+  deleteFromCart(cartId) {
+    this.cartService.deleteCartProduct(cartId).subscribe(data => {
+      this.deletedCart=data;
+      console.log(this.deletedCart.id);
+      this.userCart.forEach((element ,index)=> {
+        if (element.id===this.deletedCart.id){
+          this.userCart.splice(index,1);
+          return;
+        };
+      });
+    });
+  };
 }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
